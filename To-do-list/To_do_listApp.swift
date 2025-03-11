b885bc5a2949feb1902286c47f4d30dac64e657b
Nya James <130016910+nyjames@users.noreply.github.com>
2025-03-11 18:39:25 -0400

    Initial Commit

---
+++ To-do-list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To-do-list
+//
+//  Created by Nya James on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To-do-listTests/To_do_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listTests.swift
+//  To-do-listTests
+//
+//  Created by Nya James on 3/11/25.
+//
+
+import Testing
+@testable import To_do_list
+
+struct To_do_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
