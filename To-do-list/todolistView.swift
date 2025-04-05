15e5d3975434c9cbd9f15dd6f97d7fa966d12a1e
Nya James <130016910+nyjames@users.noreply.github.com>
2025-04-05 00:24:49 -0400

    Major UI Update
    Added folders, Quote (will be attached to API), and more!

--- To-do-list/todolistView.swift
+++ To-do-list/todolistView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct todolistView: View {
     
-// ------- Declaration -------- //
+    // ------- Declaration -------- //
     
     @State var todo:[todolistitems] = [todolistitems]()
     var dataService = DataService()
@@ -18,123 +18,358 @@ struct todolistView: View {
         if let index = todo.firstIndex(where: {$0.id == task.id }) {
             todo.remove(at: index)
         }
+        
+        func add(_ task: todolistitems) {
+            // add to
+        }
     }
+    
+    
+    
     
     var body: some View {
         
         
-// --------------------------- //
+        // --------------------------- //
+        
+        
+        
+        
+        
         
         ZStack{
-            Text("To do list")
-                .bold()
-        }
-        
-        List(todo) {task in
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            
+            
+            
+            
             
             VStack{
                 
-                Spacer()
                 
-                HStack{
-                    
-                    Text(task.title)
-                        .bold()
-                        .font(.caption)
-                    
-                    Spacer()
-                    Spacer()
-                    
-                    
-                        .font(.caption)
-                    
-                }
+                Circle()
+                    .frame(width: 405, height: 500)
+                    .offset(x: 0, y: -160)
+                    .foregroundStyle(
+                        Color(hex: "#C4C3E3")
+                    )
+                
+                
+                
+                
                 
                 Spacer()
+                
+            }
+            
+            Spacer()
+            
+            
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                
                 
                 VStack{
                     
                     Spacer()
-                    
-                    Text(task.tasks)
-                    
                     Spacer()
-                }
-                
-                Spacer()
-                
-                HStack{
+                    Spacer()
                     Spacer()
                     Spacer()
                     
-                    Button {
-                        delete(task)
-                    } label: {
-                        Image(systemName: "trash")
+                    HStack{
+                        
+                        Text("Hello Nya, Welcome Back!")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Image(systemName: "person.crop.circle.fill")
                             .resizable()
-                            .frame(width: 16, height: 18)
-                    }
-
-    
+                            .frame(width: 50, height: 50)
+                            .padding()
+                        
+                        
+                        
+                        
+                        
+                    }.padding()
+                        .foregroundStyle(
+                            Color(hex: "#504E76")
+                        )
                     
                     
-                    
-                }
-                
-                Spacer()
-                
-                
-                
-            }
-            .listRowSeparator(.automatic)
-            
-            
-            
-            
-        }
-        .listStyle(.plain)
-        .onAppear{
-            
-            todo = dataService.getData()
-            
-        }
-        
-        ZStack{
-            
-            HStack{
-                
-                Button {
-                    
-                    // pass
-                } label: {
-                    Text(" New Task ")
-                        .font(.caption)
+                    ZStack{
+                        
+                        Rectangle()
+                        
+                        Color(.white)
+                        
+                        
+                        VStack{
+                            
+                            Text("Quote of the day")
+                                .font(.title2)
+                                .bold()
+                            
+                            HStack {
+                                
+                                Text("Hello, Name! You deserve to have your life together. Act like it!")
+                                    .font(.headline)
+                                    .bold()
+                                
+                                Spacer()
+                                
+                            }.padding()
+                            
+                        }.foregroundStyle(
+                            Color(hex: "#504E76")
+                        )
+                        
+                        
+                        
+                        
+                        
+                    }.frame(width: 375, height: 200)
+                        .cornerRadius(15)
                         .padding()
-                        .border(.black)
-                        .foregroundStyle(.black)
+                        .shadow(radius: 6, x: 5, y: 5)
                     
+                    
+                    
+                    
+                    
+                    //title
+                    
+                    HStack {
+                        
+                        Text("Task Folders")
+                            .font(.title2)
+                            .bold()
+                        
+                        Spacer()
+                        
+                    }.padding()
+                        .foregroundStyle(
+                            Color(hex: "#41549a")
+                        )
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        
+                        HStack {
+                            
+                            
+                                
+                                Color(hex: "#504E76")
+                                Circle()
+                                    .frame(width: 275, height: 500)
+                                    .offset(x: 0, y: -150)
+                                    .foregroundStyle(
+                                        Color(hex: "#DDD7E5")
+                                    )
+                                
+                                VStack{
+                                    Image(systemName: "folder.fill")
+                                        .resizable()
+                                        .frame(width: 100, height: 70)
+                                        .shadow(radius: 6, x: 5, y: 5)
+                                        .padding()
+                                    
+                                    Text("School Assignments")
+                                        .font(.headline)
+                                        .padding()
+                                    
+                                }.padding()
+                                
+                                
+                            }.frame(width: 200, height: 250)
+                            
+                                .cornerRadius(25)
+                            
+                                .foregroundStyle(
+                                    Color(hex: "#F0E6E4")
+                                )
+                            
+                                .shadow(radius: 6, x: 5, y: 5)
+                            
+                            ZStack {
+                                
+                                Color(hex: "#504E76")
+                                Circle()
+                                    .frame(width: 275, height: 500)
+                                    .offset(x: 0, y: -150)
+                                    .foregroundStyle(
+                                        Color(hex: "#DDD7E5")
+                                    )
+                                
+                                VStack{
+                                    Image(systemName: "folder.fill")
+                                        .resizable()
+                                        .frame(width: 100, height: 70)
+                                        .shadow(radius: 6, x: 5, y: 5)
+                                        .padding()
+                                    
+                                    Text("School Assignments")
+                                        .font(.headline)
+                                        .padding()
+                                    
+                                }.padding()
+                                
+                                
+                            }.frame(width: 200, height: 250)
+                            
+                                .cornerRadius(25)
+                            
+                                .foregroundStyle(
+                                    Color(hex: "#F0E6E4")
+                                )
+                                .shadow(radius: 6, x: 5, y: 5)
+                            
+                            ZStack {
+                                
+                                Color(hex: "#504E76")
+                                Circle()
+                                    .frame(width: 275, height: 500)
+                                    .offset(x: 0, y: -150)
+                                    .foregroundStyle(
+                                        Color(hex: "#DDD7E5")
+                                    )
+                                
+                                VStack{
+                                    Image(systemName: "folder.fill")
+                                        .resizable()
+                                        .frame(width: 100, height: 70)
+                                        .shadow(radius: 6, x: 5, y: 5)
+                                        .padding()
+                                    
+                                    Text("School Assignments")
+                                        .font(.headline)
+                                        .padding()
+                                    
+                                }.padding()
+                                
+                                
+                            }.frame(width: 200, height: 250)
+                            
+                                .cornerRadius(25)
+                            
+                                .foregroundStyle(
+                                    Color(hex: "#F0E6E4")
+                                )
+                                .shadow(radius: 6, x: 5, y: 5)
+                        }
+                        
+                        
+                    }
+                    
+                    HStack {
+                        
+                        Text("Current tasks to complete")
+                            .font(.title2)
+                            .bold()
+                        
+                        Spacer()
+                        
+                    }.padding()
+                    
+                    VStack {
+                        ScrollView(.vertical, showsIndicators: false) {
+                            ForEach(todo) { task in
+                                
+                                
+                                
+                                
+                                ZStack{
+                                    
+                                    Rectangle()
+                                    
+                                    Color(.white)
+                                    
+                                    VStack{
+                                        
+                                        HStack{
+                                            Text(task.title)
+                                                .font(.title2)
+                                                .bold()
+                                            
+                                            Spacer()
+                                            
+                                            // Delete Button
+                                            
+                                            Button {
+                                                delete(task)
+                                            } label: {
+                                                Image(systemName: "trash")
+                                                    
+                                            }
+                                            
+                                            
+                                        }
+                                        
+                                        
+                                        HStack{
+                                            Text("Due at 3:00PM")
+                                            
+                                            Spacer()
+                                            
+                                            Text("03/21")
+                                            
+                                            
+                                        }
+                                        
+                                        Spacer()
+                            
+                                        VStack{
+                                            Text(task.tasks)
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        HStack{
+                                            
+                                            Text(task.status)
+                                            
+                                            Spacer()
+                                        }
+                                        
+                                        
+                                    }.padding()
+
+ 
+                                }.frame(width: 375, height: 200)
+                                    .cornerRadius(15)
+                                    .padding()
+                                    .shadow(radius: 6, x: 5, y: 5)
+                                
+                                
+                            }
+                        }
+                        .onAppear{
+                            todo = dataService.getData()
+                        }
+                        
+                    }
                 }
                 
-                
             }
+        
+    }.ignoresSafeArea()
+    
+    
+    
             
-            
-            
-            
-        }.padding()
+        
+  
+        
         
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            
+    
         
         
         
